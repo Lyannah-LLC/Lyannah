8b103bf306a74d4d1ec31a76d6796f5c19317f8d
Godwin Daniel <godwinolele10@gmail.com>
2022-06-27 22:02:24 +0100

    redesigned the login and register page

--- lyannah-console/src/pages/signup/signup.tsx
+++ lyannah-console/src/pages/signup/signup.tsx
@@ -7,8 +7,53 @@ import { Link } from "react-router-dom";
 
 export const SignUp = (props: any) => {
   return (
-      <div
-        className={`box-border text-xl flex md:flex-row  flex-col  ${classes.parent}  `}
+    <div className="text-xl flex justify-between items-center md:flex-row">
+      <div className={`md:w-8/12  flex`}>
+        <div className={`w-full inline-flex flex-col ${classes.formBox}`}>
+          <img src="../images/logo_black.svg" className="h-12 self-start" />
+          <p className="text-lg font-semibold mt-[4rem] mb-[1rem] text-[#333333] text-[24px]">
+            Let's get you in
+          </p>
+          <SocialAuthButton
+            buttonTitle="Signup with Github"
+            iconName="github.svg"
+          />
+          <SocialAuthButton
+            buttonTitle="Signup with Google"
+            iconName="google.svg"
+          />
+          <p className="text-[24px] font-semibold text-center my-4">or</p>
+          <Form placeHolder="Email" type="email" />
+          <Form placeHolder="Full Name" />
+          <Form placeHolder="Password" type="password" />
+          <Form placeHolder="Confirm Password" type="password" />
+          <Form placeHolder="Business Type" />
+          <Button title="Create Account" type="text" />
+          <Link to="/login">
+            <p className={`text-center text-sm font-bold underline  ${classes.color}`}>
+              Login instead ?
+            </p>
+          </Link>
+          <div className="mt-[1rem]">
+            <Link to="/forgot-password">
+              <p
+                className={`text-center text-sm font-bold underline ${classes.color}`}
+              >
+                Forgot Password ?
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <AuthLanding />
+    </div>
+  );
+};
+
+{
+  /*
+     <div
+        className="text-xl flex justify-between items-center md:flex-row  flex-col"
       >
         <div className={`md:w-8/12  flex `}>
           <div
@@ -42,6 +87,5 @@ export const SignUp = (props: any) => {
           </div>
         </div>
         <AuthLanding />
-      </div>
-  );
-};
+      </div> */
+}
