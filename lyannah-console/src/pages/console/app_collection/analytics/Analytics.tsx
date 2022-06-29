01eaf634f481fd2e240884738263c95800439f54
Fisayo <obilaja@gmail.com>
2022-06-29 17:08:46 +0100

    refact: effected tag changes accross app

--- lyannah-console/src/pages/console/app_collection/analytics/Analytics.tsx
+++ lyannah-console/src/pages/console/app_collection/analytics/Analytics.tsx
@@ -8,7 +8,7 @@ export const AnalyticsView = (props: any) => {
         <div className="mainScreen h-screen w-full max-w-screen-2xl m-auto px-10 pt-12 ">
           <div className="flex mb-10">
             <p className="text-3xl font-bold mr-2">Analytics</p>
-            <Tag title="coming soon" />
+            <Tag title="coming soon" color="blue" />
           </div>
           <div className="flex flex-col items-center self-center  ">
             <img src="../images/shipping.svg" className=" w-80 mb-10" />
