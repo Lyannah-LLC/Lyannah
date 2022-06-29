099815774c4ef01b0463a4fb9236026f023d9211
Godwin Daniel <godwinolele10@gmail.com>
2022-06-29 15:43:48 +0100

    keep proposional fixed and very compatable size on bigger screen

--- lyannah-console/src/pages/signup/signup.tsx
+++ lyannah-console/src/pages/signup/signup.tsx
@@ -7,47 +7,47 @@ import { Link } from "react-router-dom";
 
 export const SignUp = (props: any) => {
   return (
-    <div className="text-xl flex justify-between items-center md:flex-row sign-up">
-      <div className={`md:w-8/12  flex`}>
-        <div className={`w-full inline-flex flex-col ${classes.formBox}`}>
-          <img src="../images/logo_black.svg" className="h-12 self-start" />
-          <p className="font-semibold mt-[4rem] mb-[1rem] text-[#333333] text-[24px]">
-            Let's get you in
-          </p>
-          <SocialAuthButton
-            buttonTitle="Signup with Github"
-            iconName="github.svg"
-          />
-          <SocialAuthButton
-            buttonTitle="Signup with Google"
-            iconName="google.svg"
-          />
-          <p className="text-[24px] font-semibold text-center my-4">or</p>
-          <Form placeHolder="Email" type="email" />
-          <Form placeHolder="Full Name" />
-          <Form placeHolder="Password" type="password" />
-          <Form placeHolder="Confirm Password" type="password" />
-          <Form placeHolder="Business Type" />
-          <Button title="Create Account" type="text" />
-          <Link to="/login">
-            <p className={`text-center text-sm font-bold underline  ${classes.color}`}>
-              Login instead ?
+      <div className="text-xl flex justify-between items-center md:flex-row sign-up">
+        <div className={`md:w-8/12  flex`}>
+          <div className={`w-full inline-flex flex-col ${classes.formBox}`}>
+            <img src="../images/logo_black.svg" className="h-12 self-start" />
+            <p className="font-semibold mt-[4rem] mb-[1rem] text-[#333333] text-[24px]">
+              Let's get you in
             </p>
-          </Link>
-          <div className="mt-[1rem]">
-            <Link to="/forgot-password">
+            <SocialAuthButton
+              buttonTitle="Signup with Github"
+              iconName="github.svg"
+            />
+            <SocialAuthButton
+              buttonTitle="Signup with Google"
+              iconName="google.svg"
+            />
+            <p className="text-[24px] font-semibold text-center my-4">or</p>
+            <Form placeHolder="Email" type="email" />
+            <Form placeHolder="Full Name" />
+            <Form placeHolder="Password" type="password" />
+            <Form placeHolder="Confirm Password" type="password" />
+            <Form placeHolder="Business Type" />
+            <Button title="Create Account" type="text" />
+            <Link to="/login">
               <p
-                className={`text-center text-sm font-bold underline ${classes.color}`}
+                className={`text-center text-sm font-bold underline  ${classes.color}`}
               >
-                Forgot Password ?
+                Login instead ?
               </p>
             </Link>
+            <div className="mt-[1rem]">
+              <Link to="/forgot-password">
+                <p
+                  className={`text-center text-sm font-bold underline ${classes.color}`}
+                >
+                  Forgot Password ?
+                </p>
+              </Link>
+            </div>
           </div>
         </div>
+        <AuthLanding />
       </div>
-      <AuthLanding />
-    </div>
   );
 };
-
-
