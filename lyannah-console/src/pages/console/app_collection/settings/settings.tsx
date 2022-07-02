5cf409b1c7757790ae07f54d6ddec4ca2de3d719
Fisayo <obilaja@gmail.com>
2022-07-02 23:56:00 +0100

    chore: settings UI complete.

--- lyannah-console/src/pages/console/app_collection/settings/settings.tsx
+++ lyannah-console/src/pages/console/app_collection/settings/settings.tsx
@@ -7,8 +7,16 @@ export const SettingsView = (props: any) => {
       <p className="text-3xl font-bold">Settings</p>
 
       <TabBar tabChildren="" />
-      <div className="gbg-blue-10 px-10 py-5 rounded-3xl text-xs mt-6">
-        Click to copy
+      <div className="gbg-blue-10 px-10 py-5 rounded-3xl mt-6 max-w-[1000px]">
+        <p className="text-xs"> Click to copy</p>
+        <div className="rounded-md gbg-blue-30 p-2 self-start inline-block mt-6 ">
+          <img src="../images/copy.svg" className="block" />
+        </div>
+        <div className="flex mt-2 ">
+          <p className="text-xl font-bold  overflow-x-scroll text-clip">
+            xPxmzyamajfvosdjfbsunhiwuhxaihnfcaifnxixfcxPxmzyamajfvosdjfbsunhiwuhxaihnfcaifnxixfcxPxmzyamajfvosdjfbsunhiwuhxaihnfcaifnxixfcxPxmzyamajfvosdjfbsunhiwuhxaihnfcaifnxixfcxPxmzyamajfvosdjfbsunhiwuhxaihnfcaifnxixfcxPxmzyamajfvosdjfbsunhiwuhxaihnfcaifnxixfc
+          </p>
+        </div>
       </div>
     </ConsoleWrapper>
   );
