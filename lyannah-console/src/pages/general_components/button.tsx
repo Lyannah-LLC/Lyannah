487e24401b5a91b8cd571ff599193d184357f3b9
Fisayo <obilaja@gmail.com>
2022-07-04 17:35:58 +0100

    chore: button hover effect done

--- lyannah-console/src/pages/general_components/button.tsx
+++ lyannah-console/src/pages/general_components/button.tsx
@@ -4,9 +4,11 @@ export const Button = (props: any) => {
   return (
     <button
       type="submit"
-      className={`py-4 px-10 rounded-[10px] font-bold md:flex-grow-0 flex-grow ${
+      className={`py-4 px-10 rounded-[10px] font-bold md:flex-grow-0 flex-grow hover:opacity-80 transition-opacity duration-150 ${
         props.buttonType == ButtonTypes.negative ? "bg-red-700" : "bg-black"
-      } ${
+      } 
+     
+      ${
         props.width == "contain" ? null : "w-full"
       } my-8 text-white text-[16px]`}
       onClick={props.onClick}
